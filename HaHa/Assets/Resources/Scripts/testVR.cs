778064dc77c1a5e86f094ea510581771df758e12
kotomineshikionVR <547998698@qq.com>
2018-08-09 17:34:08 +0800

    te

---
+++ HaHa/Assets/Resources/Scripts/testVR.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+using Valve.VR;
+using VRTK;
+using Valve.VR.InteractionSystem;
+public class testVR : MonoBehaviour {
+    public GameObject rightController;
+	// Use this for initialization
+	void Start () {
+        rightController= VRTK_DeviceFinder.GetControllerRightHand();
+        Debug.Log(rightController);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(rightController.transform.position);
+        
+        if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.Menu))
+        {
+            Debug.Log("MenuPressed");
+        }
+        if (ViveInput.GetPressUp(HandRole.RightHand, ControllerButton.Menu))
+        {
+            Debug.Log("menu press up");
+        }
+        if (ViveInput.GetPressUp(HandRole.RightHand, ControllerButton.Trigger))
+        {
+            Debug.Log("trigger press up");
+        }
+
+       
+    }
+    public void Hit()
+    {
+        Debug.Log("Hit");
+    }
+}
